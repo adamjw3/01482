137c9422051fbd85198157048aec457849ef8da1
adam wright <adam.wright90@yahoo.co.uk>
2020-12-17 19:20:11 +0000

    updates

--- src/pages/index.js
+++ src/pages/index.js
@@ -22,8 +22,7 @@ const IndexPage = () => {
           <p>Selected partnerships: Airbnb, Apple, Anagram, BBC, Credit Suisse, Ford, Design Studio, Google, Its Nice That, Koto, Landor, LVMH, Made Thought, Moving Brands, Pentagram, Sennep, SPACE10, This Works and Wolff Olins</p>
         </div>
         <div className="items">
-          <p>Reach out if you would like to collaborate:
-         <a href="mailto:craig@01482.xyz">Craig Jackson</a></p>
+          <p>Reach out if you would like to collaborate:<br /> <a href="mailto:craig@01482.xyz">craig@01482.xyz </a></p>
         </div>
       </Container>
     </Layout >
