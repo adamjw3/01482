b54f54e7277ad9f547909ee5e5462a4af426171b
adam wright <adam.wright90@yahoo.co.uk>
2020-12-19 04:41:51 +0000

    Update to one column

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,17 +15,14 @@ const IndexPage = () => {
       <Container>
         <div className="items">
           <p>01482 is a digital design practice with over 15 years experience partering with brands,
-        individuals and studio’s. Founded by Graphic Designer <a href="mailto:craig@01482.xyz">Craig Jackson</a>
+        individuals and studio’s. Founded by Graphic Designer <a href="">Craig Jackson</a>
           </p>
         </div>
         <div className="items">
           <p>Selected partnerships: Airbnb, Apple, Anagram, BBC, Credit Suisse, Ford, Design Studio, Google, Its Nice That, Koto, Landor, LVMH, Made Thought, Moving Brands, Pentagram, Sennep, SPACE10, This Works and Wolff Olins</p>
         </div>
         <div className="items">
-          <p>Reach out if you would like to collaborate:<br /> <a href="mailto:craig@01482.xyz">craig@01482.xyz </a></p>
-        </div>
-        <div className="items">
-
+          <p>Reach out if you would like to collaborate: <a href="mailto:craig@01482.xyz">craig@01482.xyz </a></p>
         </div>
       </Container>
     </Layout >
